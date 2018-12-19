39deea7cf47c88bb7c59fca78f56e83d822d1cf8
Povilas Tamosauskas <povilast@hotmail.lt>
2018-12-19 19:42:30 +0200

    Begin third assignment

---
+++ LAB2/ProgramInplementation/MagicDB/MagicDB/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Npgsql;
+namespace MagicDB
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string connstring = "Server=127.0.0.1; Port=5432; User Id=postgres; Password=h*!J45R^sntuz; Database=MagicDB;";
+            List<string> artistNameList = new List<string>();
+            List<string> artistSurnameList = new List<string>();
+            try
+            {
+                using (NpgsqlConnection conn = new NpgsqlConnection(connstring))
+                {
+                    conn.Open();
+                    NpgsqlCommand cmd = new NpgsqlCommand("SELECT * FROM pota4187.Artist", conn);
+                    NpgsqlDataReader dr = cmd.ExecuteReader();
+                    for (int i = 0; dr.Read(); i++)
+                    {
+                        artistNameList.Add(dr[1].ToString());
+                    }
+
+                    foreach (string s in artistNameList)
+                    {
+                        Console.WriteLine(s);
+                    }
+                    dr.Close();
+                    Console.ReadKey();
+                }
+            }
+            catch (Exception msg)
+            {
+                Console.WriteLine(msg.ToString());
+                Console.ReadKey();
+                throw;
+            }
+
+        }
+    }
+}
